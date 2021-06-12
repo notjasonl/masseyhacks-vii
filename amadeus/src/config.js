ef4e6b081310315fcf0d9019873ea8b4e91f2308
Rithvik <rbhogavilli@gmail.com>
2021-06-11 22:08:37 -0400

    tsa times

--- amadeus/src/config.js
+++ amadeus/src/config.js
@@ -2,5 +2,6 @@ require('dotenv').config({path: __dirname + '/.env'})
 
 module.exports = {
     AMADEUS_KEY: process.env.AMADEUS_KEY,
-    AMADEUS_SECRET: process.env.AMADEUS_SECRET
+    AMADEUS_SECRET: process.env.AMADEUS_SECRET,
+    TSA_KEY: process.env.TSA_KEY
 }
--- amadeus/src/index.js
+++ amadeus/src/index.js
@@ -2,12 +2,13 @@ const axios = require("axios")
 var Amadeus = require("amadeus")
 const express = require("express")
 const socket = require("socket.io")
-// const bodyParser = require("body-parser")
-const { AMADEUS_KEY, AMADEUS_SECRET } = require("./config")
+const { AMADEUS_KEY, AMADEUS_SECRET, TSA_KEY } = require("./config")
 
 const PORT = process.env.PORT || 3001
 
 const app = express()
+
+const tsa_base = `https://www.tsawaittimes.com/api/`
 
 // app.use(express.urlencoded())
 app.use(express.json())
@@ -28,31 +29,51 @@ app.post("/date", async function (req, res) {
     let locArr = req.body.locationArrival
     let adults = req.body.adults
 
-    // const flights = await amadeus.shopping.flightOffersSearch.get({
-    //     originLocationCode: locDep,
-    //     destinationLocationCode: locArr,
-    //     departureDate: departure,
-    //     adults: adults
-    // }).catch((err) => {
-    //     console.log(err)
-    // })
-
-    // try {
-    //     res.json(JSON.parse(flights.body))
-    // } catch (err) {
-    //     await res.json(err)
-    // }
+    const flights = await amadeus.shopping.flightOffersSearch.get({
+        originLocationCode: locDep,
+        destinationLocationCode: locArr,
+        departureDate: departure,
+        adults: adults
+    }).catch((err) => {
+        console.log(err)
+    })
 
     try {
-        res.json({ message: "hi" })
+        res.json(JSON.parse(flights.body))
+    } catch (err) {
+        await res.json(err)
+    }
+
+    try {
+        res.json(flights.body)
     } catch (err) {
         await res.json(err)
     }
 })
 
-app.post("/price", async function (req, res) {
-    res.json(req.query)
-    console.log(req.query)
+app.post("/times", async function (req, res) {
+    let locDep = req.query.locationDeparture
+    let departure = ""
+    let time = parseInt(req.query.departure.split("T")[1].split(":")[0])
+    console.log(time)
+
+    if (time >= 12) {
+        departure = `${time % 12} pm - ${(time % 12) + 1} pm`
+    } else {
+        departure = `${time} am - ${time + 1} am`
+    }
+
+
+
+    let x = await axios.get(`${tsa_base}/airport/${TSA_KEY}/ATL/json`)
+    let delay = x.data.estimated_hourly_times.filter(slot => slot.timeslot === departure)[0]
+    console.log(delay)
+
+    try {
+        res.json({waittime: delay.waittime})
+    } catch (err) {
+        await res.join(err)
+    }
 
 })
 
