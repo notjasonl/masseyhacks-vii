3b81b31e7fa8424f942d6eb8c310557848778f94
Jason Liu <shadowbox826@gmail.com>
2021-06-12 12:29:42 -0400

    add mapclick api endpoint

--- server/index.js
+++ server/index.js
@@ -4,9 +4,14 @@ const PORT = process.env.PORT || 4001;
 
 const app = express();
 
+app.post("/api/mapClick", (req, res) => {
+  let { lat, long } = req.body
+  res.send(200)
+})
+
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
+  res.json({ message: "Hello from server!" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
