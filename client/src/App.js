c473e83d960ea552bf203825c65f67cbbe5e992e
Jason Liu <shadowbox826@gmail.com>
2021-06-11 20:30:37 -0400

    cra boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { Logo } from './Logo';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
