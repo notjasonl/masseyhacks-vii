2875b2034eb378493e8176f2c93a67c634405839
Jason Liu <shadowbox826@gmail.com>
2021-06-12 13:32:44 -0400

    push philly

--- client/src/pages/Map/index.jsx
+++ client/src/pages/Map/index.jsx
@@ -25,308 +25,162 @@ mapboxgl.accessToken =
 /**
  * Paint Constants
  */
-var config = {
-  style: "mapbox://styles/branigan/cjz37rcb003ib1cr3s8rnkt2d",
-  accessToken:
-    "pk.eyJ1IjoibWJ4c29sdXRpb25zIiwiYSI6ImNrMm01aG9hdTBlZGwzbXQ1ZXVrNHNmejAifQ.QHQA0N6XPWddCXtvoODHZg",
+ var config = {
+  style: 'mapbox://styles/branigan/cjzsvonse027m1co4nkxp13b3',
+  accessToken: 'pk.eyJ1IjoibWJ4c29sdXRpb25zIiwiYSI6ImNrMm01aG9hdTBlZGwzbXQ1ZXVrNHNmejAifQ.QHQA0N6XPWddCXtvoODHZg',
   showMarkers: false,
-  theme: "dark",
-  use3dTerrain: true,
-  title: "Glaciers of Glacier National Park",
-  subtitle: "Change in coverage from 1998 to 2015",
-  byline: "",
-  footer:
-    'Source: Story text from Wikipedia, August 2019. Data from <a href="https://www.usgs.gov/centers/norock/science/retreat-glaciers-glacier-national-park">USGS</a>',
+  theme: 'light',
+  title: '',
+  subtitle: '',
+  byline: '',
+  footer: '',
   chapters: [
-    {
-      id: "glacier-np",
-      alignment: "full",
-      title: "Glacier National Park Glaciers",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/2015-06-19_Glacier_National_Park_%28U.S.%29_8633.jpg/800px-2015-06-19_Glacier_National_Park_%28U.S.%29_8633.jpg",
-      description:
-        "Glacier National Park is dominated by mountains which were carved into their present shapes by the huge glaciers of the last ice age. These glaciers have largely disappeared over the last 12,000 years. Evidence of widespread glacial action is found throughout the park in the form of U-shaped valleys, cirques, arêtes, and large outflow lakes radiating like fingers from the base of the highest peaks. Since the end of the ice ages, various warming and cooling trends have occurred. The last recent cooling trend was during the Little Ice Age, which took place approximately between 1550 and 1850. During the Little Ice Age, the glaciers in the park expanded and advanced, although to nowhere near as great an extent as they had during the Ice Age.",
-      location: {
-        center: [-113.91666, 48.66451],
-        zoom: 8,
-        pitch: 0.0,
-        bearing: 0.0,
+      {
+          id: 'phl',
+          alignment: 'right',
+          title: 'Philadelphia Bicycle Infrastructure',
+          image: '',
+          description: 'Getting around Philadelphia on two wheels is fast, fun, and cheap. As a typical East Coast large city, the urban core is dense, so there is a lot within reach of a 15 minute ride... even mountain bike trails. Paired with the public transit infrastructure, cycling can be more efficient and much less expensive than driving (and parking) a car.',
+          location: {
+              center: [-75.13080, 39.97790],
+              zoom: 9.83,
+              pitch: 0.00,
+              bearing: 0.00
+          },
+          onChapterEnter: [
+              {
+                  layer: 'phl-city-limits',
+                  opacity: .45
+              }
+          ],
+          onChapterExit: [
+              {
+                  layer: 'phl-city-limits',
+                  opacity: 0
+              }
+          ]
       },
-      onChapterEnter: [
-        {
-          layer: "gnpglaciers-1998",
-          opacity: 0.25,
-        },
-        {
-          layer: "glaciernp-boundary",
-          opacity: 0.25,
-        },
-      ],
-      onChapterExit: [
-        {
-          layer: "glaciernp-boundary",
-          opacity: 0,
-        },
-      ],
-    },
-    {
-      id: "harrison1998",
-      alignment: "left",
-      title: "Harrison Glacier, 1998",
-      image: "",
-      description:
-        "Harrison Glacier is located in the US state of Montana in Glacier National Park. Situated on a southeast facing ridge immediately south of Mount Jackson, Harrison Glacier is the largest glacier in Glacier National Park. Compared to many of the vanishing glaciers in Glacier National Park, Harrison Glacier has a much higher altitude accumulation zone (approximately 9,000 feet (2,700 m)) which has allowed it to maintain some equilibrium in its glacier mass balance.",
-      location: {
-        center: [-113.72917, 48.58938],
-        zoom: 12.92,
-        pitch: 39.5,
-        bearing: 36.0,
+      {
+          id: 'bike-lanes',
+          alignment: 'right',
+          title: 'Bike Lanes',
+          image: '',
+          description: 'Philadelphia has XX miles of bike lanes, XX miles of which are protected. Drivers are getting more used to sharing the road, but ride defensively.',
+          location: {
+              center: [-75.13901, 39.97085],
+              zoom: 11.62,
+              pitch: 55.50,
+              bearing: -7.20
+          },
+          onChapterEnter: [
+              {
+                  layer: 'phl-bike-network',
+                  opacity: 1
+              }
+          ],
+          onChapterExit: []
       },
-      onChapterEnter: [],
-      onChapterExit: [
-        // {
-        //     layer: 'gnpglaciers-2015',
-        //     opacity: 0
-        // }
-      ],
-    },
-    {
-      id: "harrison2015",
-      alignment: "left",
-      title: "Harrison Glacier, 2015",
-      image: "",
-      description:
-        "Between 1998 and 2015, Harrison Glacier lost 169 acres of surface area (about 37%).",
-      location: {
-        center: [-113.72917, 48.58938],
-        zoom: 12.92,
-        pitch: 39.5,
-        bearing: 36.0,
+      {
+          id: 'indego',
+          alignment: 'right',
+          title: 'Indego Bike Share',
+          image: '',
+          description: 'Indego has been operating in Philadelphia since 20XX. The system initally was focused on Center City, but has expanded service to neighboring areas to support equitable mobility options to the city\'s residents.',
+          location: {
+              center: [-75.16468, 39.94503],
+              zoom: 13.15,
+              pitch: 60.00,
+              bearing: -16.80
+          },
+          onChapterEnter: [
+              {
+                  layer: 'indego-stations',
+                  opacity: 0.8
+              }
+          ],
+          onChapterExit: [
+              {
+                  layer: 'indego-stations',
+                  opacity: 0
+              }
+          ]
       },
-      onChapterEnter: [
-        {
-          layer: "gnpglaciers-2015",
-          opacity: 0.25,
-        },
-      ],
-      onChapterExit: [
-        {
-          layer: "gnpglaciers-2015",
-          opacity: 0,
-        },
-      ],
-    },
-    {
-      id: "blackfoot1998",
-      alignment: "left",
-      title: "Blackfoot Glacier, 1998",
-      image: "",
-      description:
-        "Blackfoot Glacier is the second largest of the remaining 25 glaciers in Glacier National Park, Montana. Blackfoot Glacier is just to the north of Blackfoot Mountain and near Jackson Glacier. When first documented in 1850, the glacier also included the now separate Jackson Glacier and together, they covered 1,875 acres (7.59 km2). In 1850, there were an estimated 150 glaciers in the park. Glaciologists have stated that by the year 2030, all the glaciers in the park may disappear.",
-      location: {
-        center: [-113.66573, 48.59181],
-        zoom: 12.92,
-        pitch: 51.5,
-        bearing: -26.4,
+      {
+          id: 'belmont',
+          alignment: 'right',
+          title: 'Belmont Plateau Trails',
+          image: '',
+          description: 'A short ride along the Schuylkill River Trail from the Art Museum, Belmont is a twisty, log-ridden rollercoaster of a trail network. It is easy to get turned around, the underbrush is at times impenetrable, and short steep sections come out of nowhere. In other words, it\'s really fun',
+          location: {
+              center: [-75.20325, 39.99574],
+              zoom: 14.99,
+              pitch: 44.00,
+              bearing: -40.00
+          },
+          onChapterEnter: [
+              {
+                  layer: 'belmont',
+                  opacity: 1
+              }
+          ],
+          onChapterExit: [
+              {
+                  layer: 'belmont',
+                  opacity: 0
+              }
+          ]
       },
-      onChapterEnter: [],
-      onChapterExit: [],
-    },
-    {
-      id: "blackfoot2015",
-      alignment: "left",
-      title: "Blackfoot Glacier, 2015",
-      image: "",
-      description:
-        "Between 1998 and 2015, Blackfoot Glacier lost 31 acres of surface area (about 8%).",
-      location: {
-        center: [-113.66573, 48.59181],
-        zoom: 12.92,
-        pitch: 51.5,
-        bearing: -26.4,
+      {
+          id: 'wiss',
+          alignment: 'right',
+          title: 'Wissahickon Park Trails',
+          image: '',
+          description: 'This steep, rocky gorge can be surprisingly technical. Follow the orange and yellow trails to repeatedly climb and descend through the schist hillsides (careful of the cliffs), or stick to the gravel Forbidden Drive for a relaxing ride along the creek. You\'ll forget you\'re in a city.',
+          location: {
+              center: [-75.21223, 40.05028],
+              zoom: 13.08,
+              pitch: 47.50,
+              bearing: 32.80
+          },
+          onChapterEnter: [
+              {
+                  layer: 'wissahickon',
+                  opacity: 1
+              }
+          ],
+          onChapterExit: [
+              {
+                  layer: 'wissahickon',
+                  opacity: 0
+              }
+          ]
       },
-      onChapterEnter: [
-        {
-          layer: "gnpglaciers-2015",
-          opacity: 0.25,
-        },
-      ],
-      onChapterExit: [
-        {
-          layer: "gnpglaciers-2015",
-          opacity: 0,
-        },
-      ],
-    },
-    {
-      id: "agassiz1998",
-      alignment: "left",
-      title: "Agassiz Glacier, 1998",
-      image: "",
-      description:
-        "Agassiz Glacier is in Glacier National Park in the U.S. state of Montana. It is named after Louis Agassiz, a Swiss-American glaciologist. The glacier is situated in a cirque to the southeast of Kintla Peak west of the Continental Divide. Agassiz Glacier is one of several glaciers that have been selected for monitoring by the U.S. Geological Survey's Glacier Monitoring Research program, which is researching changes to the mass balance of glaciers in and surrounding Glacier National Park.",
-      location: {
-        center: [-114.15881, 48.93439],
-        zoom: 13.51,
-        pitch: 41.0,
-        bearing: 78.33,
-      },
-      onChapterEnter: [],
-      onChapterExit: [],
-    },
-    {
-      id: "agassiz2015",
-      alignment: "left",
-      title: "Agassiz Glacier, 2015",
-      image: "",
-      description:
-        "Between 1998 and 2015, Agassiz Glacier lost 108 acres of surface area (about 37%).",
-      location: {
-        center: [-114.15881, 48.93439],
-        zoom: 13.51,
-        pitch: 41.0,
-        bearing: 78.33,
-      },
-      onChapterEnter: [
-        {
-          layer: "gnpglaciers-2015",
-          opacity: 0.25,
-        },
-      ],
-      onChapterExit: [
-        {
-          layer: "gnpglaciers-2015",
-          opacity: 0,
-        },
-      ],
-    },
-    {
-      id: "rainbow1998",
-      alignment: "left",
-      title: "Rainbow Glacier, 1998",
-      image: "",
-      description:
-        "Rainbow Glacier is in Glacier National Park in the U.S. state of Montana. The glacier is situated immediately to the east of Rainbow Peak at an elevation between 8,500 feet (2,600 m) and 8,000 feet (2,400 m) above sea level. The glacier has visible crevasses in satellite imagery. Rainbow Glacier has shown modest retreat compared to other glaciers in Glacier National Park.",
-      location: {
-        center: [-114.08659, 48.88039],
-        zoom: 13.09,
-        pitch: 50.0,
-        bearing: -53.6,
-      },
-      onChapterEnter: [],
-      onChapterExit: [],
-    },
-    {
-      id: "rainbow2015",
-      alignment: "left",
-      title: "Rainbow Glacier, 2015",
-      image: "",
-      description:
-        "Between 1998 and 2015, Rainbow Glacier lost 17 acres of surface area (about 6%).",
-      location: {
-        center: [-114.08659, 48.88039],
-        zoom: 13.09,
-        pitch: 50.0,
-        bearing: -53.6,
-      },
-      onChapterEnter: [
-        {
-          layer: "gnpglaciers-2015",
-          opacity: 0.25,
-        },
-      ],
-      onChapterExit: [
-        {
-          layer: "gnpglaciers-2015",
-          opacity: 0,
-        },
-      ],
-    },
-    {
-      id: "kintla1998",
-      alignment: "left",
-      title: "Kintla Glacier, 1998",
-      image: "",
-      description:
-        "Kintla Glacier is in Glacier National Park in the U.S. state of Montana. The glacier is situated on a plateau 2 miles (3.2 km) southwest of Kintla Peak at an elevation between 8,700 feet (2,700 m) and 7,700 feet (2,300 m) above sea level. The glacier has numerous crevasses and is actually two glaciers.",
-      location: {
-        center: [-114.18755, 48.9288],
-        zoom: 13.09,
-        pitch: 48.5,
-        bearing: 164.0,
-      },
-      onChapterEnter: [],
-      onChapterExit: [],
-    },
-    {
-      id: "kintla2015",
-      alignment: "left",
-      title: "Kintla Glacier, 2015",
-      image: "",
-      description:
-        "Between 1998 and 2015, Harrison Glacier lost 24 acres of surface area (about 10%).",
-      location: {
-        center: [-114.18755, 48.9288],
-        zoom: 13.09,
-        pitch: 48.5,
-        bearing: 164.0,
-      },
-      onChapterEnter: [
-        {
-          layer: "gnpglaciers-2015",
-          opacity: 0.25,
-        },
-      ],
-      onChapterExit: [
-        {
-          layer: "gnpglaciers-2015",
-          opacity: 0,
-        },
-      ],
-    },
-    {
-      id: "sperry1998",
-      alignment: "left",
-      title: "Sperry Glacier, 1998",
-      image: "",
-      description:
-        "Sperry Glacier is a glacier on the north slopes of Gunsight Mountain west of the Continental Divide in Glacier National Park in the U.S. state of Montana. Although many geologic features of Glacier National Park were formed during the much longer period of glaciation ending over 10,000 years ago, Sperry Glacier — like all the glaciers in the park today — is a product of the recent Little Ice Age, the period of cooler average temperatures starting in about the 13th century and concluding in the mid-19th century.",
-      location: {
-        center: [-113.75672, 48.62433],
-        zoom: 13.68,
-        pitch: 34.5,
-        bearing: 106.4,
-      },
-      onChapterEnter: [],
-      onChapterExit: [],
-    },
-    {
-      id: "sperry2015",
-      alignment: "left",
-      title: "Sperry Glacier, 2015",
-      image: "",
-      description:
-        "Between 1998 and 2015, Harrison Glacier lost 37 acres of surface area (about 16%).",
-      location: {
-        center: [-113.75672, 48.62433],
-        zoom: 13.68,
-        pitch: 34.5,
-        bearing: 106.4,
-      },
-      onChapterEnter: [
-        {
-          layer: "gnpglaciers-2015",
-          opacity: 0.25,
-        },
-      ],
-      onChapterExit: [
-        {
-          layer: "gnpglaciers-2015",
-          opacity: 0,
-        },
-      ],
-    },
-  ],
+      {
+          id: 'pennypack',
+          alignment: 'right',
+          title: 'Pennypack Park Trails',
+          image: '',
+          description: 'Pennypack is a great introduction trail system. Not too steep and not too technical, the beautiful wooded park also provides a great escape from urban life. The south side trails are originally bridle trails, so be nice to equestrians and dismount when you approach them.',
+          location: {
+              center: [-75.05685, 40.06839],
+              zoom: 13.73,
+              pitch: 43.50,
+              bearing: 96.80
+          },
+          onChapterEnter: [
+              {
+                  layer: 'pennypack',
+                  opacity: 1
+              }
+          ],
+          onChapterExit: [
+              {
+                  layer: 'pennypack',
+                  opacity: 0
+              }
+          ]
+      }
+  ]
 };
 
 const layerTypes = {
