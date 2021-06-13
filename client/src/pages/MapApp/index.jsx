eefa3a143cc297b6adba2e3cdddeda65c2ebd40d
chiumax <dumblole@gmail.com>
2021-06-13 09:52:55 +0000

    radius animation

--- client/src/pages/MapApp/index.jsx
+++ client/src/pages/MapApp/index.jsx
@@ -64,14 +64,19 @@ const MapApp = () => {
 
   const { isOpen, onOpen, onClose } = useDisclosure();
   const mapContainer = useRef();
-  const [map, setMap] = useState();
-  const [marker, setMarker] = useState();
+
   const [lng, setLng] = useState(-77.0369);
   const [lat, setLat] = useState(38.9072);
   const [zoom, setZoom] = useState(11);
 
+  const [map, setMap] = useState();
+  const [marker, setMarker] = useState();
   const [coords, setCoords] = useState();
   const [address, setAddress] = useState();
+
+  const [regionCenter, setRegionCenter] = useState();
+  const [regionPolygon, setRegionPolygon] = useState();
+  const [filteredGeoPoints, setFilteredGeoPoints] = useState();
 
   const [zoomed, setZoomed] = useState(true);
 
@@ -124,8 +129,83 @@ const MapApp = () => {
     });
     const markerElm = new mapboxgl.Marker();
     mapElm.on("load", () => {
+      const size = 500;
+      const pulsingDot = {
+        width: size,
+        height: size,
+        data: new Uint8Array(size * size * 4),
+
+        // When the layer is added to the map,
+        // get the rendering context for the map canvas.
+        onAdd: function () {
+          var canvas = document.createElement("canvas");
+          canvas.width = this.width;
+          canvas.height = this.height;
+          this.context = canvas.getContext("2d");
+        },
+
+        // Call once before every frame where the icon will be used.
+        render: function () {
+          var duration = 1000;
+          var t = (performance.now() % duration) / duration;
+
+          var radius = (size / 2) * 0.3;
+          var outerRadius = (size / 2) * 0.7 * t + radius;
+          var context = this.context;
+
+          // Draw the outer circle.
+          context.clearRect(0, 0, this.width, this.height);
+          context.beginPath();
+          context.arc(
+            this.width / 2,
+            this.height / 2,
+            outerRadius,
+            0,
+            Math.PI * 2
+          );
+          context.fillStyle = "rgba(255, 200, 200," + (1 - t) + ")";
+          context.fill();
+
+          // Update this image's data with data from the canvas.
+          this.data = context.getImageData(0, 0, this.width, this.height).data;
+
+          // Continuously repaint the map, resulting
+          // in the smooth animation of the dot.
+
+          mapElm.triggerRepaint();
+
+          // Return `true` to let the map know that the image was updated.
+          return true;
+        },
+      };
       setMap(mapElm);
       setMarker(markerElm);
+
+      mapElm.addImage("pulsing-dot", pulsingDot, { pixelRatio: 2 });
+
+      mapElm.addSource("dot-point", {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [
+            {
+              type: "Feature",
+              geometry: {
+                type: "Point",
+                coordinates: [0, 0], // icon position [lng, lat]
+              },
+            },
+          ],
+        },
+      });
+      mapElm.addLayer({
+        id: "layer-with-pulsing-dot",
+        type: "symbol",
+        source: "dot-point",
+        layout: {
+          "icon-image": "pulsing-dot",
+        },
+      });
       // getCoordFromAddress("12424 Horseshoe Bend Cir, Clarksburg, Maryland");
     });
 
@@ -136,6 +216,18 @@ const MapApp = () => {
   useEffect(() => {
     if (map) {
       marker.setLngLat([coords.lng, coords.lat]).addTo(map);
+      map.getSource("dot-point").setData({
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: [coords.lng, coords.lat], // icon position [lng, lat]
+            },
+          },
+        ],
+      });
       map.flyTo({
         duration: 2000,
         center: [coords.lng, coords.lat],
